46fb2edb5565454651dc1295c539d4cea77d69c4
lookatthisdoode <radchenko.andreii@gmail.com>
2024-02-13 21:36:59 +0300

    new approach

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react'
-import Dash from './components/dash'
+import Dash from './components/Dash'
 import Laps from './components/Laps'
 import LoginPage from './components/LoginPage'
 import { formatLapTime, generateRandomID } from './helpers'
-
-
 
 function App() {
   const [rpms, setRpms] = useState(0)
@@ -15,6 +13,9 @@ function App() {
   const [bestLap, setBestLap] = useState('--:--:--')
   const [loggedIn, setLoggedIn] = useState(false)
   const [currentCar, setCurrentCar] = useState('')
+  var HOST = location.origin.replace(/^http/, 'ws')
+  // const API_URL = 'ws://asseto-corsa-dashboard-api-b61e575ec14b.herokuapp.com'
+  // // const API_URL = 'ws://192.168.31.162:5000'
 
   let socket = null
   let maxRpm = 0
@@ -38,10 +39,10 @@ function App() {
     // Check if currentCar is not null
     if (currentCar !== null) {
       // Update startTime to the current timestamp
-      startTime = Date.now();
-      console.log('Start time updated:', startTime);
+      startTime = Date.now()
+      console.log('Start time updated:', startTime)
     }
-  }, [currentCar]); // Run this effect whenever currentCar changes
+  }, [currentCar]) // Run this effect whenever currentCar changes
 
   const mapRange = (value, inMin, inMax, outMin, outMax) => {
     return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
@@ -49,32 +50,27 @@ function App() {
 
   // document.documentElement.style.setProperty('--background-color', 'yellowgreen');
 
-
   const updateValues = (data) => {
-
     //lastLap
     const currentLap = formatLapTime(data.data.lapTime)
     const bestLap = formatLapTime(data.data.bestLap)
     const rpms = parseInt(data.data.engineRPM)
 
     // Set up highest rpm it seen only in first 10 seconds
-    if ((Date.now() - startTime) < 10000) {
+    if (Date.now() - startTime < 10000) {
       // Collects and sets highest rpm value for the last 10 seconds
       maxRpm = rpms > maxRpm ? rpms : maxRpm
       // Only logs it at the last second of these 10 seconds
-      if ((Date.now() - startTime) > 9900 ) {
-        console.log(`Max RPM set to: ${maxRpm}`);
-
+      if (Date.now() - startTime > 9900) {
+        console.log(`Max RPM set to: ${maxRpm}`)
       }
     }
 
-    
     const gear = gearsList[data.data.gear]
     const speed = parseInt(data.data.speedKmh)
     const gauge = parseInt(mapRange(rpms, 0, maxRpm, 0, 100))
     // console.log(rpms,gear,speed, gauge);
 
-  
     //update state for each data
     setRpms(rpms)
     setGear(gear)
@@ -84,88 +80,70 @@ function App() {
     setBestLap(bestLap)
   }
 
-
   const connectToAc = () => {
     // This IP is where the asseto corsa running (getting from user)
     const ip = document.querySelector('.login-ip-form').value
-    if (!ip) return 
+    if (!ip) return
 
     // this IP is where this server will be running
-    socket = new WebSocket('ws://192.168.31.162:3000')
-
+    socket = new WebSocket(HOST)
 
     socket.onopen = () => {
-
-      //Setloading to wait untill connection
-
-
       // Once socket is open send initial ID and pass IP to try to connect to AC
-      socket.send(JSON.stringify({ id: generateRandomID(), method: 'connection', ip: ip }))
-     
-    
+      socket.send(
+        JSON.stringify({ id: generateRandomID(), method: 'connection', ip: location.origin })
+      )
+
       // Add listeners to newly opened socket
-      socket.onmessage = async (event) => {
-        const data = await event.data
-        const parsedData = await JSON.parse(data)
+      socket.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data)
 
         switch (parsedData.method) {
           case 'handshake':
             // Set new starting timer by updating car
             setCurrentCar(parsedData.data.carName)
-             // Set Flag to render actual display instead of 'login' stuff
+            // Set Flag to render actual display instead of 'login' stuff
             setLoggedIn(true)
-            break;
+            break
           case 'data':
             updateValues(parsedData)
-            break;
+            break
           case 'error':
             // Display error
-            console.log(parsedData.message);
+            console.log(parsedData.message)
             // Then clear socket
             socket = null
             // Reset login trigger to see 'Enter IP screen'
             setLoggedIn(false)
           default:
-            break;
+            break
         }
       }
 
-      // Not sure if its working and how to test it 
+      // Not sure if its working and how to test it
       socket.onerror = (error) => {
-        console.error(error);
+        console.error(error)
       }
-
     }
-  } 
-
-
+  }
 
   window.addEventListener('beforeunload', function (event) {
     // send ws request to close udp server
-    socket && socket.send(
-      JSON.stringify({
-        method: 'disconnect',
-      })
-    )
+    socket &&
+      socket.send(
+        JSON.stringify({
+          method: 'disconnect',
+        })
+      )
   })
 
-  return (
-    !loggedIn ? 
-      <LoginPage 
-        connectToAc = {connectToAc}
-      />  : 
-      <div className="app">
-        <Dash 
-          rpms = {rpms}
-          gear = {gear}
-          gauge = {gauge}
-          speed = {speed}
-        />
-        <Laps 
-        currentLap = {currentLap}
-        bestLap = {bestLap}
-        />
-      </div> 
+  return !loggedIn ? (
+    <LoginPage connectToAc={connectToAc} />
+  ) : (
+    <div className="app">
+      <Dash rpms={rpms} gear={gear} gauge={gauge} speed={speed} />
+      <Laps currentLap={currentLap} bestLap={bestLap} />
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,28 +1,15 @@
 const ACRemoteTelemetryClient = require('ac-remote-telemetry-client')
 const express = require('express')
-const http = require('http')
-const WebSocket = require('ws')
-require('dotenv').config()
+const { Server } = require('ws')
+const path = require('path');
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
 
-
-
-
-// Sending broadcast data handler
-// function broadcastPhysics(data) {
-//   // Sends values to everyone connected to WS
-//   wss.clients.forEach((ws) => {
-//     if (ws.ACClient != null) {
-//       ws.send(JSON.stringify({method: 'data', data: data}))
-//     }
-//   })
-// }
-
-
-function connectToAC(ip, ws) {
+function connectToAC(ip, id, ws) {
   // Connect to UDP
   ws.ACClient = new ACRemoteTelemetryClient(ip)
+  ws.ACClient.id = id
 
-  // If something wrong with UDP (wrong IP etc)
   ws.ACClient.client.on('error', (error) => {
     const errorMsg = `Error trying to connect to AC UDP, most likely, bad IP. ${error}`
     // Send back message to client
@@ -31,15 +18,16 @@ function connectToAC(ip, ws) {
     console.log(`${ws.id} couldn't conect to AC, killing WS`);
     ws.terminate()
   }) 
-
+  
   // Implement desired listeners only when recieved handshake responce (OK)
   ws.ACClient.on('HANDSHAKER_RESPONSE', (data) => {
     data && console.log(`ID ${ws.id} successfully connected to AC UDP`)
-    ws.send(JSON.stringify({method: 'handshake', data: data}))
-
+    ws.send(JSON.stringify({ method: 'handshake', data: data }))
     // Only when its handshaked, apply another listener
-    ws.ACClient.on('RT_CAR_INFO', (data) => ws.send(JSON.stringify({method: 'data', data: data})))
-    
+  })
+
+  ws.ACClient.on('RT_CAR_INFO', (data) => {
+    ws.send(JSON.stringify({ method: 'data', data: data }))
   })
 
   // Start listening
@@ -48,34 +36,32 @@ function connectToAC(ip, ws) {
   // Send initial handshake
   ws.ACClient.handshake()
 
-    // Subscribe to desired updates
-    // ws.ACClient.subscribeSpot()
-    ws.ACClient.subscribeUpdate()
+  // Subscribe to desired updates
+  // ws.ACClient.subscribeSpot()
+  ws.ACClient.subscribeUpdate()
 }
 
-const app = express()
-app.use(express.static('public'))
-const server = http.createServer(app)
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000.')
-})
+const server = express()
+  .use(express.static(path.join(__dirname, '../client/dist')))
+  .use(cors())
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new WebSocket.Server({ server })
+const wss = new Server({ server })
+
 
 wss.on('connection', (ws) => {
-  
   ws.on('message', (message) => {
-    // Parse data 
+    // Parse data
     const data = JSON.parse(message)
     switch (data.method) {
       case 'connection':
-        // Upon connection ws gets unique ID 
+        // Upon connection ws gets unique ID
         ws.id = data.id
         console.log(`Client connected to WebSocket, id: ${ws.id}`)
         console.log(`Trying to connect to AC UPD at ${data.ip}`)
         // And attaches AC Client to ws
-        connectToAC(data.ip, ws)
+        connectToAC(data.ip, data.id, ws)
         break
       case 'disconnect':
         console.log(`${ws.id} requested disconnect...`)
@@ -83,7 +69,7 @@ wss.on('connection', (ws) => {
         ws.ACClient && ws.ACClient.dismiss()
         // Then kill WS
         ws.terminate()
-        console.log(`${ws.id} disconnected`);
+        console.log(`${ws.id} disconnected`)
         break
       default:
         break
@@ -93,8 +79,6 @@ wss.on('connection', (ws) => {
   ws.on('error', (error) => {
     console.log('WebSocket error:', error.message)
   })
-
-  
 })
 
 // When i stop the app, close conenctions etc
@@ -106,7 +90,6 @@ process.on('SIGINT', () => {
     ws.ACClient && ws.ACClient.dismiss()
     ws.terminate()
   })
-  
+
   process.exit()
 })
-
